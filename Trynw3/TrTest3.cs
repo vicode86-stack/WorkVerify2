fcb1107b0abd66362831221ad599e6ccc18604bf
vi <vscode86@gmail.com>
2025-12-25 16:44:03 +0530

    Add new project and update solution file

---
+++ Trynw3/TrTest3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trynw3
+{
+    internal class TrTest3
+    {
+    }
+}
